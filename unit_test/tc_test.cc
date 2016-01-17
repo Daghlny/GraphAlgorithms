601bfd654181b7e9f1ecefca370d83f2ff6f0f42
Daghlny <woainimanlian@163.com>
2016-01-17 14:07:16 +0300

    add clock of tc algorithm

--- unit_test/tc_test.cc
+++ unit_test/tc_test.cc
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<ctime>
 #include"../basic/uGraph.hpp"
 #include"../algorithms/algorithms_types.hpp"
 
@@ -6,12 +7,18 @@ size_t tc_for_ugraph(std::set<tri_t> *res, uGraph* ug);
 
 int
 main(int argc, char **argv){
+	long beg_time = std::clock();
 	uGraph* ug = new uGraph();
 	ug->read_graph_data(argv[1]);
+	long end_time = std::clock();
+	std::cout << "the time of building graph: " << (end_time-beg_time)/CLOCKS_PER_SEC << std::endl;
 	
 	std::set<tri_t>* res = new std::set<tri_t>();
+	beg_time = std::clock();
 	size_t tri_num = tc_for_ugraph(res, ug);
+	end_time = std::clock();
 
 	std::cout << tri_num << std::endl;
+	std::cout << "Running time: " << (end_time-beg_time)/CLOCKS_PER_SEC << std::endl;
 	return 1;
 }
