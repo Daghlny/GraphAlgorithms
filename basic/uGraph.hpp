f3f09b68adb3756477bdae0ac09bbf52b82029ce
Daghlny <woainimanlian@163.com>
2016-01-18 22:23:14 +0300

    add Graph file

--- basic/uGraph.hpp
+++ basic/uGraph.hpp
@@ -7,6 +7,8 @@
 #include<string>
 
 #include"types.hpp"
+#include"Graph.hpp"
+
 //#include"Graph.hpp"
 
 typedef std::map<vid_t, adj_t> ug_t;
@@ -14,7 +16,7 @@ typedef std::map<vid_t, adj_t> ug_t;
 // the objects of this class represent a undirected graph
 // the class supply some basic operations
 // data of a uGraph connot be changed after the building phase
-class uGraph{
+class uGraph: public Graph{
 	public:
 		uGraph(){}
 		uGraph(char *file){read_graph_data(file);}
